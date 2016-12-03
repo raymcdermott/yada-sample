e0958e9e3f1463aaaf70a1799805e3d20a3d7af4
Ray McDermott <ray@mcdermott.be>
2016-12-03 17:33:07 +0100

    names are more idiomatic code DRYer

--- src/com/starch/commmands_processor.clj
+++ src/com/starch/commmands_processor.clj
@@ -4,45 +4,28 @@
             [taoensso.timbre :as timbre :refer [debug info warn error fatal]])
   (:import (java.util UUID Date)))
 
-(def ^:private system-origin {:type :system})
-
 (def ^:private version (UUID/randomUUID))
 
-(defn- create-transfer-event [transfer-resource]
+; event creators
+
+(defn- create-event
+  [resource event]
   {:id            (UUID/randomUUID)
-   :relationships (:relationships transfer-resource)
    :time-stamp    (System/currentTimeMillis)
    :version       version
-   :origin        system-origin
-   :resource      {:id   (:id transfer-resource),
-                   :href (str ps/api-url "transfers/" (:id transfer-resource))}})
+   :event         event
+   :relationships (:relationships resource)
+   :origin        (:origin resource)               ; not as our spec - but more useful / correct [I think]
+   :resource      {:id   (:id resource),
+                   :href (str ps/api-url "transfers/" (:id resource))}}) ; hmmm, who likes a hard coded path??
 
-(defn- create-initiated-transfer-event
-  [transfer-resource]
-  (merge (create-transfer-event transfer-resource)
-         {:event  :transferInitiated
-          :origin (:origin transfer-resource)}))            ; not as spec - but correct I think
-
-(defn- create-expired-transfer-event
-  [transfer-resource]
-  (merge (create-transfer-event transfer-resource)
-         {:event  :transferClaimExpired
-          :origin (:origin transfer-resource)}))
-
-(defn- create-failed-transfer-event
-  [ctx]
-  (let [transfer-resource (get-in ctx [:response :body])]
-    (ps/publish-event (merge (create-transfer-event transfer-resource)
-                             {:event   :transferFailed
-                              :context {:reason :INSUFFICIENT_FUNDS}})))
-  ctx)
-
-; create a transfer resource, store it and return an initiated event
-(defn- process-create-transfer-command
-  [transfer-command]
-  (let [amount (get-in transfer-command [:context :amount])
-        source (get-in transfer-command [:context :customer-source])
-        target (get-in transfer-command [:context :customer-target])
+; create a transfer resource, store it
+(defn- create-transfer
+  [command]
+  (let [command-id (:id command)
+        amount (get-in command [:context :amount])
+        source (get-in command [:context :customer-source])
+        target (get-in command [:context :customer-target])
         transfer-resource {:id            (UUID/randomUUID)
                            :time-stamp    (System/currentTimeMillis)
                            :version       version
@@ -50,54 +33,67 @@
                            :attributes    {:amount amount}
                            :relationships {:customer-source {:data  {:type :customer
                                                                      :id   source}
-                                                             :links {:related (str ps/api-url "customers/" source)}
+                                                             :links {:related (str ps/api-url "customers/" source)} ; hmmm, who likes a hard coded path??
                                                              }
                                            :customer-target {:data  {:type :customer
                                                                      :id   target}
-                                                             :links {:related (str ps/api-url "customers/" target)}
+                                                             :links {:related (str ps/api-url "customers/" target)} ; hmmm, who likes a hard coded path??
                                                              }}
                            :origin        {:type :command
-                                           :id   (:id transfer-command)
-                                           :href (str ps/api-url "commands/" (:id transfer-command))}}]
+                                           :id   command-id
+                                           :href (str ps/api-url "commands/" command-id)}}] ; hmmm, who likes a hard coded path??
     (ds/store-transfer! transfer-resource)))
 
 
-(defn- process-expire-transfer-command
-  [transfer-command]
-  (let [transfer-id (get-in transfer-command [:context :transfer-id])]
+(defn- update-transfer
+  [command update-keys update-val]
+  (let [transfer-id (get-in command [:context :transfer-id])]
     (if-let [transfer (ds/lookup-transfer transfer-id)]
-      (let [updated-attributes (assoc-in transfer [:attributes :expired] true)
+      (let [command-id (:id command)
+            updated-attributes (assoc-in transfer update-keys update-val)
             updated-meta-data {:time-stamp (System/currentTimeMillis)
                                :version    version
                                :origin     {:type :command
-                                            :id   (:id transfer-command)
-                                            :href (str ps/api-url "commands/" (:id transfer-command))}}]
+                                            :id   command-id
+                                            :href (str ps/api-url "commands/" command-id)}}] ; hmmm, who likes a hard coded path??
         (ds/store-transfer! (merge updated-attributes updated-meta-data)))
-      (error "Failed to find transfer to expire" {:command transfer-command}))))
+      (error "Cannot find transfer to update" {:command command}))))
 
-(defn- handle-create-transfer
+(defn- expire-transfer
   [command]
-  (if-let [transfer-resource (process-create-transfer-command command)]
-    (create-initiated-transfer-event transfer-resource)
-    (ex-info "Failed to handle create-transfer command" {:command command})))
+  (update-transfer command [:attributes :expired] true))
 
-(defn- handle-expire-transfer
+(defn- fail-transfer
   [command]
-  (if-let [transfer-resource (process-expire-transfer-command command)]
-    (create-expired-transfer-event transfer-resource)
-    (ex-info "Failed to handle expire-transfer command" {:command command})))
+  (update-transfer command [:attributes :failed] true))
 
-(def command->function
-  {:create-transfer handle-create-transfer
-   :expire-transfer handle-expire-transfer})
 
-(defn transfer-command-processor
+; Implement the pattern:
+; - process the command on a resource (CRUD)
+; - obtain the processed resource
+; - publish the associated event
+(defn- command-processor
+  [command command-processor event]
+  (if-let [resource (command-processor command)]
+    (create-event resource event)
+    (ex-info "Failed to process command" {:command command})))
+
+; First draft of the map; might get larger as we flesh out design
+(def command->function->event
+  {:create-transfer {:fn    create-transfer
+                     :event :transferInitiated}
+   :expire-transfer {:fn    expire-transfer
+                     :event :transferClaimExpired}
+   :fail-transfer   {:fn    fail-transfer
+                     :event :transferFailed}})
+
+(defn command-channel-processor
   "Dispatch the appropriate processor for the command on the channel"
-  [function-mapping]
+  [command-processor-map]
   (ps/command-listener (map (fn [command]
-                              (if-let [handler (get command->function (:command command))]
-                                (handler command)
-                                (ex-info "Cannot map command to a handler" {:command command :map function-mapping}))))))
+                              (if-let [map-entry (get command-processor-map (:command command))]
+                                (command-processor command (:fn map-entry) (:event map-entry))
+                                (ex-info "Cannot map command to a function" {:command command :map command-processor-map}))))))
 
 
 
